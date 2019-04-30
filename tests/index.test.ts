4f37a771192b772694a2f4b35f6d30709b1a5cd3
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2019-05-01 00:48:43 +0900

    Add test of base connection process

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,24 @@
-import { helloWorld } from '../src/index'
+import Obniz from '../src/index'
 
-test('basic', () => {
-  expect(helloWorld('John')).toBe('Hello John')
-})
-
-test('Failed', () => {
-  expect(helloWorld('')).toBe('Hello ')
+describe('Obniz', () => {
+  describe('connecting', () => {
+    test('disconnect', () => {
+      const obniz = new Obniz('0000-0000')
+      obniz
+        .connect()
+        .then(() => {
+          console.log(obniz.isConnecting.toString())
+          expect(obniz.isConnecting).toBe(true)
+          obniz
+            .disConnect()
+            .then(() => expect(obniz.isConnecting).toBe(false))
+            .catch(e => {
+              throw Error(e)
+            })
+        })
+        .catch(e => {
+          throw Error(e)
+        })
+    })
+  })
 })
